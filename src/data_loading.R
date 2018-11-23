99c305934048896fbca1f5d7bff1ae8c8c08ee7b
fan0521 <fan_nie@hotmail.com>
2018-11-23 08:58:58 -0800

    made changes on the arguments

--- src/data_loading.R
+++ src/data_loading.R
@@ -1,12 +1,13 @@
 # data_loading.R
-# Fan and Makk , November 2018
+# Fan and Makk, November 2018
 # crime_csv_all_years.csv
 
 # This script downloads the dataset "crime_csv_all_years.zip" from 
-# "ftp://webftp.vancouver.ca/opendata/csv/crime_csv_all_years.zip", and unzip the .zip file
-# This script takes two arguments: the url of the data file, and the absolute file path to save the unzipped file into.
+# "ftp://webftp.vancouver.ca/opendata/csv/crime_csv_all_years.zip", and unzip the zipped csv file and save it locally.
 #
-# Usage: Rscript data_loading.R "ftp://webftp.vancouver.ca/opendata/csv/crime_csv_all_years.zip" "C:/Users/fanfan/Documents/block3/DSCI_522_project/DSCI_522_Vancouver_Bike_Theft_Analysis/data"
+# This script takes two arguments: the path to the input file , and a path where to write the file to. 
+#
+# Usage: Rscript data_loading.R "ftp://webftp.vancouver.ca/opendata/csv/crime_csv_all_years.zip" "data"
 #
 
 library(tidyverse)
