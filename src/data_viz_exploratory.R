86144774f993f33506d8f1ffcd727fe81629c995
fan0521 <fan_nie@hotmail.com>
2018-11-23 11:51:04 -0800

    made changes on data exploratory viz rscript

---
+++ src/data_viz_exploratory.R
@@ -0,0 +1,38 @@
+# data_viz_exploratory.R
+# November 2018
+
+# This script reads the data from the second script and creates an exploratory visualization that is useful to help the
+# reader/consumer understand that dataset. 
+
+# This script takes two arguments: a path/filename pointing to the data, a path/file name where to write the figure to and what to call it 
+#
+# Usage: Rscript data_viz_exploratory.R "C:/Users/fanfan/Documents/block3/DSCI_522_project/DSCI_522_Vancouver_Bike_Theft_Analysis/data/data_all_years_bicycle.csv"  "C:/Users/fanfan/Documents/block3/DSCI_522_project/DSCI_522_Vancouver_Bike_Theft_Analysis/results/viz_exploratory.png" 
+#
+# define main function
+
+library(tidyverse)
+library(ggplot2)
+
+# read in command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+file_input <- args[1]
+img_output <- args[2]
+
+# read the clean data csv file into r
+data_all_years_bicycle <- read.csv(file_input) 
+
+main <- function(){
+  
+  p <- data_all_years_bicycle %>% 
+            group_by(YEAR,MONTH) %>%
+            summarise(n=n()) %>% # count the numbers of bicycle thefts reported in each month in each year
+            ggplot(aes(MONTH, n)) +
+            geom_line() +
+            ylab("number of bicycle theft") +
+            facet_wrap(~ YEAR, scales = "free", ncol = 3)
+  ggsave(img_output, p, device = "png")
+}
+
+
+# call main function
+main()
